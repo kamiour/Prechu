a318188fe8dc49375df20f79928e9f963605473d
Mikhail Kazakov <mikhailkazakov@inner.directory>
2019-07-12 12:35:01 +0300

    added mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+$(document).ready(function(){
+
+	var swiper = new Swiper('.swiper-container', {
+	  navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	  },
+	});
+
+	$('.burger-menu').on('click', function(e){
+		e.preventDefault();
+		$('.nav-mobile ul').toggleClass('is-open')
+	});
+  
+})
